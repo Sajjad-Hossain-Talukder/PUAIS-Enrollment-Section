24f36786d685d8d60c4d32cc25328734b56b1e17
Sajjad-Hossain-Talukder <sajjadhossain.cse35@gmail.com>
2022-08-04 02:51:22 +0600

    added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('teacher/login',function(){
+    return view('teacher.login');
+});
